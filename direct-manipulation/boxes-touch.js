1f7459a740d61212e060c03dbfab00d1cabe484c
dondi <dondi@lmu.edu>
2014-12-14 17:28:10 -0800

    Inline comments and feedback for HW 1204a (direct manipulation).

--- direct-manipulation/boxes-touch.js
+++ direct-manipulation/boxes-touch.js
@@ -1,4 +1,4 @@
-var boxIDs = [];
+var boxIDs = []; // JD: 6
 
 var drawingBoxes = [];
 
@@ -19,7 +19,7 @@ var BoxesTouch = {
             // Event handler setup must be low-level because jQuery
 
             //Adds event listener to drawing area in general
-
+            // JD: 7
             //element.addEventListener("touchstart", BoxesTouch.startDraw, false);
             //.on("touchstart", BoxesTouch.setBoxIDs)
             //.on("touchstart", BoxesTouch.startDraw)
@@ -35,6 +35,7 @@ var BoxesTouch = {
 
                 element.addEventListener("touchstart", BoxesTouch.setBoxIDs, false);
                 element.addEventListener("touchstart", BoxesTouch.startDraw, false);
+                // JD: 7 ...etc.
                 //.on("touchmove", BoxesTouch.trackDrag, false)
                 //.on("touchend", BoxesTouch.endDrag, false);
             })
@@ -50,7 +51,8 @@ var BoxesTouch = {
 
     setBoxIDs: function(event){
         $.each(event.changedTouches, function (index, touch) {
-            var ID = touch.identifier;
+            var ID = touch.identifier; // JD: 8
+            // JD: 7
             //console.log("hello");
             //console.log(touch);
             boxIDs.push(ID);
@@ -79,9 +81,10 @@ var BoxesTouch = {
         //setBoxIDs(event);
         $.each(event.changedTouches, function (index, touch) {
             if (!touch.target.movingBox) {
-
+                // JD: 7
                 //console.log(this);
 
+                // JD: 9
                 drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorX = touch.pageX;
                 drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorY = touch.pageY;
                 //console.log($("#drawing-area").anchorX);
@@ -89,6 +92,7 @@ var BoxesTouch = {
                 drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox = $("<div></div>")
                     .appendTo($("#drawing-area"))
                     .addClass("box")
+                    // JD: 10
                     .offset({left: drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorX, top: drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorY})
                     .attr("id", touch.timestamp);
 
@@ -116,24 +120,27 @@ var BoxesTouch = {
                     var touchY = touch.pageY;
                     var touchedBoxWidth = $(touch.target).width();
                     var touchedBoxHeight = $(touch.target).height();
-                    var trash
-                    if (touchX + touchedBoxWidth/2 > 520 && touchY + touchedBoxHeight/2 > 520) {
+                    var trash // JD: 11
+                    if (touchX + touchedBoxWidth/2 > 520 && touchY + touchedBoxHeight/2 > 520) { // JD: 12
                         $(touch.target).addClass("trash-highlight");
                     }
                     if (touchX + touchedBoxWidth/2 < 520 || touchY + touchedBoxHeight/2 < 520) {
                         $(touch.target).removeClass("trash-highlight");
                     }
-                }else if (drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox) {
+                }else if (drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox) { // JD: 4, 9
                     //console.log("tracking");
                     // Calculate the new box location and dimensions.  Note how
                     // this might require a "corner switch."
                     var newOffset = {
+                        // JD: 10, 9
                         left: (drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorX < touch.pageX) ? drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorX : touch.pageX,
+                        // JD: 10, 9
                         top: (drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorY < touch.pageY) ? drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorY : touch.pageY
 
-                };
+                }; // JD: 13
                     drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox
                         .offset(newOffset)
+                        // JD: 9
                         .width(Math.abs(touch.pageX - drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorX))
                         .height(Math.abs(touch.pageY - drawingBoxes[boxIDs.indexOf(touch.identifier)].anchorY));
             }
@@ -163,13 +170,13 @@ var BoxesTouch = {
                     $(touch.target).remove();
                 }
                 touch.target.movingBox = null;
-            }else if (drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox) {
+            }else if (drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox) { // JD: 4, 9
                 console.log(drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox)
 
                 drawingBoxes[boxIDs.indexOf(touch.identifier)].drawingBox
-                    .touchend(BoxesTouch.unhighlight)
+                    .touchend(BoxesTouch.unhighlight) // JD: 3
                     //.on("touchend", BoxesTouch.unhighlight)
-                    .touchstart(BoxesTouch.startMove)
+                    .touchstart(BoxesTouch.startMove) // JD: 3
                     //.on("touchstart", BoxesTouch.startMove);
                     .addEventListener("touchstart", BoxesTouch.startMove, false)
                     .addEventListener("touchend", BoxesTouch.unhighlight, false);
@@ -179,11 +186,12 @@ var BoxesTouch = {
                 //for()
 
             }
-            if(boxIDs.indexOf(touch.identifier) != -1){
+            if(boxIDs.indexOf(touch.identifier) != -1){ // JD: 4
                 boxIDs.splice(boxIDs.indexOf(touch.identifier), 1);
                 drawingBoxes.splice(boxIDs.indexOf(touch.identifier), 1);
             }
 
+            // JD: 7
             //if (touch.target.movingBox.offset()  === $("#trash-bin").offset()) {
             //    $("#trash-bin", event).droppable(touch.target.remove());
             //    console.log("Bye!!");
@@ -253,6 +261,7 @@ var BoxesTouch = {
         event.stopPropagation();
     }
 
+    // JD: 7
     //createBox: function(event) {
     //    if(event.target = "#drawing-area"){
     //        console.log("Hello!");
